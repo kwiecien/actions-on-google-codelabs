9a0e61e26ce1ef050b3aad57c68242e93977c455
kwiecien <k.d.kwiecinski@gmail.com>
2019-10-09 22:35:34 +0200

    customize responses with user information

--- level2/functions/index.js
+++ level2/functions/index.js
@@ -27,8 +27,11 @@ const app = dialogflow({debug: true});
 // The intent collects a parameter named 'color'.
 app.intent('favorite color', (conv, {color}) => {
     const luckyNumber = color.length;
-    // Respond with the user's lucky number and end the conversation.
-    conv.close('Your lucky number is ' + luckyNumber);
+    if (conv.data.userName) {
+        conv.close(`${conv.data.userName}, your lucky number is ${luckyNumber}.`);
+    } else {
+        conv.close(`Your lucky number is ${luckyNumber}.`);
+    }
 });
 
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
@@ -37,6 +40,19 @@ app.intent('Default Welcome Intent', (conv) => {
         context: 'Hi there, to get to know you better',
         permissions: 'NAME',
     }));
+});
+
+// Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
+// agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
+app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
+    if (!permissionGranted) {
+        conv.ask(`Ok, no worries. What's your favorite color?`);
+        conv.ask(new Suggestions('Blue', 'Red', 'Green'));
+    } else {
+        conv.data.userName = conv.user.name.display;
+        conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color?`);
+        conv.ask(new Suggestions('Blue', 'Red', 'Green'));
+    }
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
